5c52e4a48b652173673579c9062d86d46a8f7927
jojoa8209 <jal82tuc@gmail.com>
2025-08-14 06:53:36 -0300

    Agrega productos del 5 al 10 y corrige la logica de la funcion obtenerProductos

--- src/services/productosService.js
+++ src/services/productosService.js
@@ -1,14 +1,16 @@
-//Contiene funcion que se conecta con el archivo productos.json y maneja la respuesta.
 const obtenerProductos = async () => {
   try {
-    const response = await fetch ('/data/productos.json');
+    const response = await fetch('/data/productos.json');
+    console.log('Código de estado de la respuesta:', response.status);
     if (!response.ok) {
       throw new Error('Error al obtener los datos.');
     }
+
     const data = await response.json();
     return data;
+    
   } catch (error) {
-    console.error('Hubo un problema con la operación fetch:', error);
+    console.error('Hubo un problema con la función fetch:', error);
     return [];
   }
 };
