5110d84e3c4ef900f96a882b65660fec20a4f07d
jojoa8209 <jal82tuc@gmail.com>
2025-09-17 06:31:02 -0300

    Sprint 6 Miercoles transformacion ProductDetailPage

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from 'react';
 //importacion para Sprint 4 dia Miercoles useParams
 import { useParams } from 'react-router-dom';
-import { obtenerProductos } from '../services/productosService'; 
+//Sprint 6 comento la lista completa vendra por props.
+//import { obtenerProductos } from '../services/productosService'; 
 
-function ProductDetailPage() {
+function ProductDetailPage({productos}) {
  //devuelve un objeto con los parametros de la URL.
   const { id } = useParams();
   const [producto, setProducto] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [relacionados, setRelacionados] = useState([]);
 
+  //Sprint 6 UseEffect, se encarga de encontrar el producto principal.
+  useEffect(() =>{
+  //Si la lista principal de productos ya esta disponible.
+  if(productos.length > 0) {
+  setLoading(true);//Se inicia la  carga
+  const productoEncontrado = productos.find((p) => p.id == id);
+  setProducto(productoEncontrado);
+  setLoading(false); //Aqui termina la  carga.
+}
+},[id, productos]);// se ejecuta si cambia el ID en la URL o bien si llega la lista de productos.
+
+//Sprint 6 segundo UseEffect que se va a encargar de encontrar los productos relacionados.
+useEffect(()=>{
+// Sprint 6 si ya he encontrado el producto y esta la lista completa....
+if(producto && productos.length > 0){
+const productosFiltrados = productos.filter(p => p.genero === producto.genero && p.id !== producto.id);
+setRelacionados(productosFiltrados.slice(0, 4,));
+};
+}, [producto, productos]);//se va a ejecutar si el prod. principal cambia.
+
+  /* Sprint 6 comento,  ya no utilizo funcion asincronica, es mas eficiente la mejora solo va a recibir los datos por props
+  del componente padre App.jsx.
   useEffect(() => {
    // Funcion asincrona para cargar el producto 
     const cargarProducto = async () => {
@@ -25,7 +49,7 @@ function ProductDetailPage() {
     };
 
     cargarProducto();
-  }, [id]);
+  }, [id]); */
 
   // Renderizado condicional mientras carga
   if (loading) {
