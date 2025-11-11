4ff55e94904ac3d637a39fd2f45fe75037a78c38
jojoa8209 <jal82tuc@gmail.com>
2025-11-11 06:19:57 -0300

    feat(responsive): Aplica media queries a Home

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProductCard from './ProductCard'; 
 
-// 1. Aqui  recibo las nuevas props: toggleFavorito y favoritos
 const ProductList = ({ productos, agregarAlCarrito }) => {
   
   if (!productos || productos.length === 0) {
@@ -9,7 +8,7 @@ const ProductList = ({ productos, agregarAlCarrito }) => {
   }
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
       {productos.map(producto => (
         <ProductCard 
           key={producto.id} 
