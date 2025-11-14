0444cdc37a559bb482db2009fe9e08aca1ef9a09
jojoa8209 <jal82tuc@gmail.com>
2025-11-14 17:52:23 -0300

    cambio de ultima hora, sobre agregados y nuevos

--- src/utils/isProductNew.js
+++ src/utils/isProductNew.js
@@ -4,5 +4,5 @@ const fechaProducto = new Date(fechaAgregado);
 const hoy = new Date();
 const diferenciaTiempo = hoy.getTime() - fechaProducto.getTime();
 const diferenciaDias = diferenciaTiempo / (1000 * 3600 * 24);
-return diferenciaDias <= 7;
+return diferenciaDias <= 30;
 };
